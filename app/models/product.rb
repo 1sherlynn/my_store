4749af5c503578f102da3aa7626745a80598502e
Sherlynn Tan <1sherlynn@gmail.com>
2017-10-06 10:33:36 +0800

    add Product Model

---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ApplicationRecord
+
+	mount_uploader :avatar, AvatarUploader
+	validates :title, :description, presence: true 
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true 
+end
