2257975e74c0261e4283faca4478356782061965
Sherlynn Tan <1sherlynn@gmail.com>
2017-10-12 21:49:35 +0800

    heroku domain config for fb

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ Rails.application.initialize!
     ActionMailer::Base.smtp_settings = {
       :user_name => ENV['SENDGRID_USERNAME'],
       :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'shopping-cart-rails.herokuapp.com',
+      :domain => 'https://shopping-cart-rails.herokuapp.com',
       :address => 'smtp.sendgrid.net',
       :port => 587,
       :authentication => :plain,
