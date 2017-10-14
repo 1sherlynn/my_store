c1cf21d957dcfd9e4fcba6cb72e2a2593769b333
Sherlynn Tan <1sherlynn@gmail.com>
2017-10-14 19:10:58 +0800

    config S3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,18 +1,5 @@
 CarrierWave.configure do |config|
   
-  # Use local storage if in development or test
-  if Rails.env.development? || Rails.env.test?
-    CarrierWave.configure do |config|
-      config.storage = :file
-    end
-  end
-  
-  # Use AWS storage if in production
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.storage = :fog
-    end
-  end
   
   config.fog_credentials = {
     :provider               => 'AWS',                             # required
