3bba4ee1d8a0e9510c011abc457f899709720497
Sherlynn Tan <1sherlynn@gmail.com>
2017-10-14 18:55:07 +0800

    edit config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -16,12 +16,13 @@ CarrierWave.configure do |config|
   
   config.fog_credentials = {
     :provider               => 'AWS',                             # required
-    :aws_access_key_id      => 'AKIAIKT76JVNZCDDUECA',            # required
-    :aws_secret_access_key  => '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',     # required
-    :region                 => 'ap-southeast-1'                        # optional, defaults to 'us-east-1'
+    :aws_access_key_id      => 'S3_KEY',            # required
+    :aws_secret_access_key  => 'S3_SECRET',     # required
+    :region                 => 'S3_REGION'                        # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'shopping-cart-rails'               # required
+  config.fog_directory  = 'S3_BUCKET'               # required
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   #config.fog_public     = false                                  # optional, defaults to true
 
 end
+
